84b43a86e967f2b54de27b6ceae90eef6efed93f
Namju Kim <cmsong111@naver.com>
2022-11-27 16:28:46 +0900

    naver static map image 저장하기 완료

---
+++ app/src/main/java/HTTP/NaverMapAPI.java
@@ -0,0 +1,66 @@
+package HTTP;
+
+import DTO.StoreDTO;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class NaverMapAPI {
+    JSONParser parser = new JSONParser();
+
+    public String getStaticMAP(double locationX, double locationY) throws URISyntaxException, IOException, ParseException {
+
+        Reader reader = new FileReader("./NaverAPI-KEY.json");
+
+        JSONObject jsonObject = (JSONObject) parser.parse(reader);
+        reader.close();
+
+        HttpClient Client = HttpClientBuilder.create().build();
+        String baseURL = "https://naveropenapi.apigw.ntruss.com/map-static/v2/raster";
+
+        // URL 생성
+        HttpGet httpget = new HttpGet(baseURL);
+        URI uri = new URIBuilder(httpget.getURI())
+                .addParameter("w", "500")
+                .addParameter("h", "300")
+                .addParameter("center", String.valueOf(locationX) + "," + String.valueOf(locationY))
+                .addParameter("level", "14")
+                .addParameter("scale", "2")
+                .build();
+        httpget.setURI(uri);
+        httpget.setHeader("X-NCP-APIGW-API-KEY-ID",jsonObject.get("X-NCP-APIGW-API-KEY-ID").toString());
+        httpget.setHeader("X-NCP-APIGW-API-KEY",jsonObject.get("X-NCP-APIGW-API-KEY").toString());
+
+        // HTTP GET method 실행
+        HttpResponse response = Client.execute(httpget);
+
+
+        BufferedInputStream bInStr = new BufferedInputStream(response.getEntity().getContent()); // Buffered Stream 이용: fast down
+
+        BufferedOutputStream bOutStr = new BufferedOutputStream(new FileOutputStream(new File("app/build/resources//main/locationImage.jpg"))); // Buffered  Stream
+        int inpByte;
+        while ((inpByte = bInStr.read()) != -1) {
+            bOutStr.write(inpByte);
+        }
+
+        bInStr.close();
+        bOutStr.close();
+
+
+
+
+        return "http get 완료";
+    }
+}
