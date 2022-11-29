f7d8817f15d7ba1826599f34706a7c200f717a73
Namju Kim <cmsong111@naver.com>
2022-11-29 12:00:41 +0900

    유저 정보 업데이트, 탈퇴 HTTP Method 추가

--- app/src/main/java/HTTP/LogInHTTP.java
+++ app/src/main/java/HTTP/LogInHTTP.java
@@ -17,6 +17,13 @@ import java.security.NoSuchAlgorithmException;
 public class LogInHTTP {
     Gson gson = new Gson();
 
+    /**
+     * @param userDTO (Not null : ID,Password)
+     * @return 완성된 UserDTO
+     * @throws IOException
+     * @throws NoSuchAlgorithmException
+     * @author 김남주
+     */
     public UserDTO login(UserDTO userDTO) throws IOException, NoSuchAlgorithmException {
 
         //URL 생성
@@ -43,6 +50,13 @@ public class LogInHTTP {
         }
     }
 
+    /**
+     * @param userDTO (ID, Password, name)
+     * @return 완성된 UserDTO
+     * @throws IOException
+     * @throws NoSuchAlgorithmException
+     * @author 김남주
+     */
     public UserDTO create(UserDTO userDTO) throws IOException, NoSuchAlgorithmException {
         //URL 생성
         CloseableHttpClient client = HttpClients.createDefault();
@@ -66,6 +80,69 @@ public class LogInHTTP {
             return UserDTO.builder().upk(0L).build();
         } else {
             return testest;
+        }
+    }
+
+    /**
+     * 유저 정보 업데이트 하는 메소드입니다.
+     * UPK랑 꼭 같이 보내주세요
+     *
+     * @param userDTO
+     * @return UserDTO
+     * @throws NoSuchAlgorithmException
+     * @throws IOException
+     */
+    public UserDTO editUser(UserDTO userDTO) throws NoSuchAlgorithmException, IOException {
+        //URL 생성
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost(SingleTon.getBaseURL() + "/user/update");
+        //비밀번호 암호화
+        userDTO.setPassword(encrypt(userDTO.getPassword()));
+        // BODY 담기
+        String json = gson.toJson(userDTO);
+        StringEntity entity = new StringEntity(json, "UTF-8");
+        httpPost.setEntity(entity);
+        httpPost.setHeader("Content-Type", "application/json");
+
+        // HTTP POST 요청하기
+        HttpResponse response = client.execute(httpPost);
+        String body = EntityUtils.toString(response.getEntity());
+
+        UserDTO testest = gson.fromJson(body, UserDTO.class);
+        System.out.println(testest.toString());
+
+        if (testest.getUpk() == 0L) {
+            return UserDTO.builder().upk(0L).build();
+        } else {
+            return testest;
+        }
+    }
+
+    /**
+     * 회원 탈퇴 UPK 필수
+     *
+     * @param userDTO
+     * @return success : true, fail : false
+     */
+    public boolean withdrawal(UserDTO userDTO) throws IOException {
+        //URL 생성
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost(SingleTon.getBaseURL() + "/user/delete");
+
+        // BODY 담기
+        String json = gson.toJson(userDTO);
+        StringEntity entity = new StringEntity(json, "UTF-8");
+        httpPost.setEntity(entity);
+        httpPost.setHeader("Content-Type", "application/json");
+
+        // HTTP POST 요청하기
+        HttpResponse response = client.execute(httpPost);
+        String body = EntityUtils.toString(response.getEntity());
+
+        if (body.equals("Delete Done")) {
+            return true;
+        } else {
+            return false;
         }
     }
 
